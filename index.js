c65f9eb06c2f628f4db7fe2f6159ee6f5ca51475
Serena Chandler <serenachandler2019@gmail.com>
2021-04-26 16:53:49 -0700

    added function to allow user to update pre existing employees

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ function addRole() {
         },
       ])
       .then((answer) => {
-        console.log(answer)
+        console.log(answer);
         connection.query(
           "INSERT INTO role (title, salary, department_id) values (?, ?, ?)",
           [answer.name, answer.salary, answer.department],
@@ -132,7 +132,7 @@ function addEmployee() {
     const names = res.map((row) => {
       return { name: row.title, value: row.id };
     });
-    console.log(names);
+
     inquirer
       .prompt([
         {
@@ -151,7 +151,7 @@ function addEmployee() {
         },
       ])
       .then((answers) => {
-        console.log(answers)
+        console.log(answers);
         connection.query(
           "INSERT INTO employee (first_name, last_name, role_id) values (?, ?, ?)",
           [answers.firstName, answers.lastName, answers.role],
@@ -164,33 +164,33 @@ function addEmployee() {
   });
 }
 
-function viewDepartment() {  connection.query(
-  `SELECT name AS department FROM department`,
-  (err, res) => {
+function viewDepartment() {
+  connection.query(`SELECT name AS department FROM department`, (err, res) => {
     if (err) throw err;
     console.table(res);
     start();
-  }
-);}
+  });
+}
 
-function viewRoles() {  
+function viewRoles() {
   connection.query(
     `SELECT title, salary, name
     FROM department
     LEFT JOIN role ON department.id = role.department_id`,
-  (err, res) => {
-    if (err) throw err;
-    console.table(res);
-    start();
-  }
-);}
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      start();
+    }
+  );
+}
 
 function viewEmployee() {
   connection.query(
     `SELECT e.first_name, e.last_name, title, name AS department, salary, concat(m.first_name, " ",  m.last_name ) AS manager 
     FROM department
     LEFT JOIN role ON department.id = role.department_id
-    LEFT JOIN employee e ON role.id = e.role_id
+    RIGHT JOIN employee e ON role.id = e.role_id
     LEFT JOIN employee m ON e.id = m.manager_id;
     `,
     (err, res) => {
@@ -198,6 +198,53 @@ function viewEmployee() {
       console.table(res);
       start();
     }
-  );}
+  );
+}
 
-function UpdateEmployeeRoles() {}
+function UpdateEmployeeRoles() {
+  connection.query(`SELECT title, id FROM role`, (err, res) => {
+    if (err) throw err;
+    const roles = res.map((row) => {
+      return { name: row.title, value: row.id };
+    });
+
+    connection.query(
+      `SELECT first_name, last_name, role_id FROM employee`,
+      (err, res) => {
+        if (err) throw err;
+        const names = res.map((row) => {
+          return {
+            name: row.first_name + " " + row.last_name,
+            value: row.role_id,
+          };
+        });
+
+     
+
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              message: "Which employee would you like to update?",
+              name: "employee",
+              choices: names,
+            },
+            {
+              type: "list",
+              message: "Which role does this employee need?",
+              name: "role",
+              choices: roles,
+            },
+          ])
+          .then((answer) => {
+            
+            connection.query(`UPDATE employee SET role_id = ? WHERE role_id = ?`, [
+              answer.role,
+              answer.employee,
+            ]);
+            start();
+          });
+      }
+    );
+  });
+}
--- seeds.sql
+++ seeds.sql
@@ -7,6 +7,7 @@ INSERT INTO role (title, salary, department_id)
 values ("Sales Lead",100000, 1), ("Salesperson", 80000, 1), ("Lead Engineer", 150000, 2), ("Software Engineer", 120000, 2), ("Lead Accountant", 175000, 3), ("Accountant", 125000, 3), ("Legal Team Lead", 250000, 4), ("lawyer", 190000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-values ("joey", "johnson", 2, 2), ("john", "Joseph", 1, NULL), ("sarah", "Karen", 3, NULL), ("Mike", "Chan", 4, 3), 
+values ("joey", "johnson", 2, 2), ("john", "joseph", 1, NULL), ("sarah", "Karen", 3, NULL), ("Mike", "Chan", 4, 3), 
 ("Tammer", "Galal", 5, NULL), ("Malia", "Brown", 6, 5), ("Kevin", "Tupik", 7, NULL), ("Tom", "Allen", 8, 7);
+
 
